bb909b9623bbdb44b4ecc68f40c1cc99e3c2a947
Ellison Song <ellsong@ucdavis.edu>
2025-05-28 04:56:40 -0700

    Basic login functionality and session fetching proof of concept

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,64 +1,70 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import svelteLogo from './assets/svelte.svg';
-  import viteLogo from '/vite.svg';
-  import Counter from './lib/Counter.svelte';
-
-  let apiKey: string = '';
-
+  
   onMount(async () => {
+    // const res = await fetch('/getinfo');
+    // const data = await res.json();
+    // console.log(data);
     try {
-      const res = await fetch('/api/key');
+      const res = await fetch('/api/getinfo');
       const data = await res.json();
-      apiKey = data.apiKey;
+      console.log(data);
     } catch (error) {
-      console.error('Failed to fetch API key:', error);
+      console.error('Failed to fetch info:', error);
     }
   }); 
+  /**
+   * function to redirect to the login page
+   */
+  function redirectToLogin() {
+    window.location.href = "http://localhost:8000/login";
+  }
+  /**
+   * function to redirect to the logout page
+   */
+  function redirectToLogout() {
+    window.location.href = "http://localhost:8000/logout";
+  }
+  
+  
+  async function getinfo() {
+    try {
+      const res = await fetch('/api/getinfo');
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Failed to fetch info:', error);
+    }
+  }
+
+  async function testFetch() {
+    try {
+      const res = await fetch('/api/testfetch');
+      // console.log(res);
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Failed fetch test:', error);
+    }
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Your API Key: <strong>{apiKey}</strong>
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <button onclick={redirectToLogin}>
+    TESTBUTTON LOGIN
+  </button>
+  <button onclick={redirectToLogout}>
+    TESTBUTTON LOGOUT
+  </button>
+  <button onclick={getinfo}>
+    TESTBUTTON GETINFO
+  </button>
+  <button onclick={testFetch}>
+    TESTBUTTON TESTFETCH
+  </button>
+  
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  
 </style>
