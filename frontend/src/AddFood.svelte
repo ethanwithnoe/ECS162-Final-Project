b861ad09728233c7c5cdf0a3d4b25459fae2ac6a
imqttt <matthewhoey10@gmail.com>
2025-06-08 19:50:30 -0700

    Added the addfood table to meals page so users can add there.

--- frontend/src/AddFood.svelte
+++ frontend/src/AddFood.svelte
@@ -4,6 +4,7 @@
     let searchQuery = "";
     let searchResults: any[] = [];
     let chosenFood: any = null;
+    let visibleCount = 10;
     let userMacros = {
         name: "",
         calories: 0,
@@ -32,7 +33,7 @@
         }
             const data = await res.json();
             console.log("search results:", data)
-            searchResults = data.foods.slice(0, 50); 
+            searchResults = data.foods.slice(0, 100); 
         } catch (e) {
             console.error("Network Error:", e);
             searchResults = [];
@@ -59,27 +60,36 @@
         });
         const result = await res.json();
         console.log("Saved:", result);
+        visibleCount = 5;
         if(onFoodAdded) onFoodAdded(result);
     }
 
     function nutrientByName(item: any, nutrientName: string) { 
         return item.foodNutrients.find((n: any) => n.nutrientName === nutrientName)?.value || "?";
     }
-
+    function viewMore() {
+        visibleCount = visibleCount + 10;
+    }
 </script>
 
-<div> 
-    <h2> Add Food of Choice! </h2>
-    <input type="text" bind:value={searchQuery} placeholder="Search food..." />
-    <button on:click={foodSearch}>Search</button>
-    <button on:click = {() => {
-        chosenFood = { description: "Enter Manually" };
-        manualMode = true;
-    }}>+ Add Food Manually</button>
+    
+<div class="table-wrap">
 
+    <button class="exit" on:click={() => onFoodAdded(null)}>X</button>
+    <h2> Add Food of Choice! </h2>
+    
+    <div class="search">
+        <input type="text" bind:value={searchQuery} placeholder="Search food..." />
+        <button on:click={foodSearch}>Search</button>
+        <button on:click = {() => {
+            chosenFood = { description: "Enter Manually" };
+            manualMode = true;
+        }}>+ Add Food Manually</button>
+    </div>
+    
     {#if searchResults.length > 0} 
         <ul>
-            {#each searchResults as item}
+            {#each searchResults.slice(0, visibleCount) as item}
                 <li on:click={() => selectFood(item)}>
                     <strong>{item.brandName ? item.brandName + ":" : ""}</strong> {item.description}<br /> 
                     Calories:       {nutrientByName(item, "Energy" )} kcal |
@@ -89,6 +99,9 @@
                 </li>
             {/each}
         </ul>
+        {#if visibleCount < searchResults.length}
+                    <button on:click={viewMore}>View More</button>
+        {/if}
     {:else} 
         <p>No results</p>
     {/if}
@@ -130,5 +143,40 @@
     }
     td {
         padding: 5px;
+    }
+    .table-wrap {
+        position: relative;
+        padding: 20px;
+        border: 1px solid black;
+        border-radius: 8px;
+        background-color: rgba(0, 0, 0, 0.219);
+        border: 1px solid rgb(26, 72, 109);
+        border-radius: 8px;
+        width: 50%;
+        margin: 20px auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 10px;
+    }
+    .search {
+        display: flex;
+        flex-direction: row;
+        gap: 10px; 
+        margin-bottom: 15px;
+        justify-content: center;
+    }
+    .exit {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        background: rgb(109, 109, 109);
+        color: white;
+        font-size: 1.2rem;
+        border: 1px solid black;
+        cursor: pointer;
+    }
+    .exit:hover {
+        color: red;
     }
 </style>
